bd83455f5f1a689adc19e0cf27a08fc69f6aafec
daniarzubanys2-star <daniarzubanys2@gmail.com>
2026-07-26 21:55:03 +0500

    Daniyar

--- src/KthSmallestFinder.java
+++ src/KthSmallestFinder.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main1 {
+public class KthSmallestFinder {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
